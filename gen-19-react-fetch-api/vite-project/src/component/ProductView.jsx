58824a090802032bbc664123441bb26e1d6e0ba1
Dewi Ayu <deasa092@gmail.com>
2024-02-15 11:59:53 +0700

    ubah view produk dengan data json menggunakan swr

--- gen-19-react-fetch-api/vite-project/src/component/ProductView.jsx
+++ gen-19-react-fetch-api/vite-project/src/component/ProductView.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function ProductDetail(props) {
-   const {image, preview} = props;
+   const {image, preview, id} = props;
 
    const [mainImage, setMainImage] = useState(image);
+
+   useEffect(() => {
+      window.scrollTo(0, 0);
+     setMainImage(image);
+   }, [id]);
 
    function changeMainImage(view) {
       setMainImage(view);
@@ -14,7 +19,7 @@ function ProductDetail(props) {
             <img src={mainImage} alt="Preview-Product" className="w-[420px]" />
          </div>
          <div className="flex justify-center gap-4">
-            {preview.map((view,index) => (
+            {preview?.map((view,index) => (
                <div className='img-view-option'>
                   <div className={view === mainImage ? 'img-view-select' : ''}>
                      <img
