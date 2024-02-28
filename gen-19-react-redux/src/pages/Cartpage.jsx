4768e9dd5b98b72a8b4e7dfee856a30c65a01040
Dewi Ayu <deasa092@gmail.com>
2024-02-29 01:39:25 +0700

    update Cartpage

--- gen-19-react-redux/src/pages/Cartpage.jsx
+++ gen-19-react-redux/src/pages/Cartpage.jsx
@@ -3,6 +3,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { toRupiah } from "../utils/formatter";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, remove, removeOne } from "../redux/action/action";
+import { Link } from "react-router-dom";
 
 function Cartpage() {
   
@@ -13,39 +14,52 @@ function Cartpage() {
 
   return (
     <div className="text-center ">
-      <h1 className="section-title m-5">Cart Page</h1>
+      <Link to={"/"}><h1 className="section-title m-5">Cart Page</h1></Link>
+      <div className="grid grid-cols-6 text-xl gap-10 items-center th-style mx-10">
+        <div></div>
+        <div>Product</div>
+        <div>Price</div>
+        <div>Quantity</div>
+        <div>Total Price</div>
+        <div></div>
+      </div>
       <div>
         {cart.length === 0 && <p>No Data in Cart</p>}
-        <div className="m-5 p-5">
+        <div className="mx-5 px-5 ">
           {cart.map((item) => (
             <div
               key={item.id}
-              className="grid grid-cols-6 text-xl gap-10 items-center"
+              className="grid grid-cols-6 text-xl gap-10 items-center  border-b border-gray-500 rounded-lg"
             >
-              <div>
-                <img src={item.image} className="w-24 my-3" />
+              <div className="flex justify-center">
+                <img src={item.image} className="w-24 my-3 rounded-md" />
               </div>
               <div className="">{item.name}</div>
               <div>{toRupiah(item.price)} </div>
               <div className="flex gap-4 items-center">
                 <button
-                  className="bg-slate-200 py-2 px-4 rounded-lg"                 
-                  onClick={item.qty.count === 1 ? () => dispatch(remove(item.id)) : () => dispatch(removeOne(item))}>
+                  className="bg-slate-200 py-2 px-4 rounded-lg"
+                  onClick={
+                    item.qty === 1
+                      ? () => dispatch(remove(item.id))
+                      : () => dispatch(removeOne(item))
+                  }
+                >
                   -
                 </button>
-                <div className="">{item.qty.count}</div>
+                <div className="">{item.qty}</div>
                 <button
                   className="bg-slate-200 py-2 px-4 rounded-lg"
-                 onClick={()=>dispatch(addToCart(item))}
+                  onClick={() => dispatch(addToCart(item))}
                 >
                   +
                 </button>
               </div>
-              <div>{toRupiah(item.price * item.qty.count)}</div>
+              <div>{toRupiah(item.price * item.qty)}</div>
               <div>
                 <button
                   onClick={() => dispatch(remove(item.id))}
-                  className="p-3 bg-red-600 text-white rounded-md"
+                  className="p-3 bg-red-600 text-white  hover:bg-red-700 regular-btn"
                 >
                   Delete
                 </button>
